437ccf95eb1383515f9995256d07fa238826873c
kaljegk <00001094771892sp@al.educacao.sp.gov.br>
2024-11-23 03:33:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Para que serve o Python?',
+    'Para criar sites, jogos, automações e muito mais'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Foi Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'É uma sequência de passos para resolver um problema'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz BOM DIA em inglês?',
+    'Bom dia em ingles é GOOD MORNING'
+) 
